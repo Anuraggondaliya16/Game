0aa4e597caf5dd68277127aa8504365b1dcf5bc5
Anuraggondaliya16 <er.anurag3026@gmail.com>
2024-06-01 01:15:03 +0530

    First Commit

---
+++ Rock,Paper,Scissors Game/Game.js
@@ -0,0 +1,64 @@
+let userScore = 0;
+let compScore = 0;
+
+const choices = document.querySelectorAll(".choice");
+const msg = document.querySelector("#msg");
+const userScorePara = document.querySelector("#user-score");
+const compScorePara = document.querySelector("#comp-score");
+
+const genCompChoice = () => {
+    const option = ["rock", "paper", "scissors"];
+    const randIdx = Math.floor(Math.random() * 3);
+    return option[randIdx]; 
+};
+
+const drawGame = () => {
+    msg.innerText = "You Game Was Draw! Play Again..";
+    msg.style.backgroundColor = "#081b31";
+};
+
+const showWinner = (userWin) => {
+    if(userWin) {
+        userScore++;
+        userScorePara.innerText = userScore;
+        msg.innerText = "You Win!";
+        msg.style.backgroundColor = "green";
+    }else{
+        compScore++;
+        compScorePara.innerText = compScore;
+        msg.innerText = "You Lose!";
+        msg.style.backgroundColor = "red";
+    }
+};
+
+const playGame = (userChoice) => {
+    console.log("userChoice = ", userChoice);
+    //genrate computer choice
+    const compChoice = genCompChoice();
+    console.log("comp Choice = ", compChoice);
+
+    if (userChoice === compChoice) {
+        //draw game
+        drawGame();
+    }else {
+        let userWin = true;
+        if (userChoice === "rock"){
+            //scissors, paper 
+            userWin = compChoice === "paper" ? false : true;
+        }else if (userChoice === "paper"){
+            //rock,scissors
+            userWin = compChoice === "scissors" ? false : true;
+        }else{
+            //rock,paper
+            userWin = compChoice === "rock" ? false : true
+        }
+        showWinner(userWin);
+    }
+};
+
+choices.forEach((choice) => {
+    choice.addEventListener("click", () => {
+        const userChoice = choice.getAttribute("id");
+        playGame(userChoice);
+    });
+});
---
+++ Rock,Paper,Scissors Game/script.js
@@ -0,0 +1,85 @@
+// let score = 95;
+
+// if(score >= 90 && score <= 100){
+//     Grade = "A";
+// }else if (score >=70 && score <= 89){
+//     Grade = "B";
+// }else if (score >= 60 && score <= 69){
+//     Grade = "B";
+// }else if (score >= 50 && score <= 59){
+//     Grade = "B";
+// }else if(score >= 0 && score <= 49){
+//     Grade = "B";
+// }        
+
+// console.log("according to your scores, your grade was : ", Grade);
+
+
+
+
+
+/**pq1**/
+
+// for(let num=0; num<=100; num++) {
+    
+//     if(num%2 === 0) {
+//         console.log("num =", num);
+//     }
+// }
+
+/**pq2**/
+
+
+// let gameNum = 25;
+
+// let userNum = prompt ("Guess the game Number : ");
+
+// while (userNum !== gameNum) {
+
+// }
+
+/**pq3**/
+
+// let fullName = prompt("enter your full name without spaces");
+
+// let userName = "@" + fullName + fullName.length;
+
+// console.log(userName);
+
+/**pq4**/
+
+// let marks = [85, 78, 98, 95, 82, 66];
+
+// let sum = 0;
+
+// for( let val of marks) {
+//    sum += val;
+
+// }
+// let avg = sum / marks.length;
+// console.log(avg);
+
+/**pq4**/
+
+// let items = [250, 645, 300, 900, 50];
+
+// let i = 0;
+// for (let val of items) {
+//     console.log(`value at index ${i} = ${val}`);
+//     let offer = val/10;
+//     items[i] = items[i]-offer;
+//     console.log(`value after offer =${items[i]}`);
+//     i++;
+
+// }
+
+
+/**pq4**/
+
+// let companies = ["bloomberg", "microsoft", "Uber", "Google", "IBM", "netflix"];
+
+//companies.shift();
+
+// companies.splice(2, 1, "ola");
+
+// companies.push("Amazon");
